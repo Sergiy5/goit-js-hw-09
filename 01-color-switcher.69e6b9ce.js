ecf72954cb310763d6e910a869623c434030dae2
Sergiy5 <114180707+Sergiy5@users.noreply.github.com>
2023-03-10 13:36:01 +0000

    Deploying to gh-pages from @ Sergiy5/goit-js-hw-09@ddf47a04cd2c6dff9cc81fa9c9603917836c8075 🚀

---
+++ 01-color-switcher.69e6b9ce.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(){0!==o&&(t.disabled=!0);o=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));n.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.69e6b9ce.js.map
