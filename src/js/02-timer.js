b4f90a9c52849724935a3945c5a1b7894b79ece6
Sergiy Kushnir <s1983kushnir@gmail.com>
2023-03-11 13:59:45 +0200

    Clear second

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -19,65 +19,82 @@ const options = {
   onClose(selectedDates) {
     if (selectedDates[0] < options.defaultDate) {
       btnStart.disabled = true;
-    return Notify.info('Please choose a date in the future', {
-  width: '280px',
-  position: 'center-top',
-});
-    }
+          return notifyInfo();
+    };
+
+    btnStart.addEventListener('click', () => { timer.start() });
+
     btnStart.disabled = false;
 
     const timer = {
-      isActive: false,
+      //isActive Для того щоб повторно не включити таймер як що кнопку не відключати
+      // isActive: false,
       intervalId: null,
       start() {
-        if (this.isActive) {
-          return;
-        }
+        // if (this.isActive) {
+        //   return;
+        // }
         const startTime = selectedDates[0];
-        this.isActive = true;
+        // this.isActive = true;
+
         this.intervalId = setInterval(() => {
           btnStart.disabled = true;
-          const currentTime = Date.now()
+          const currentTime = Date.now();
           const deltaTime = startTime - currentTime;
-          
-          if (deltaTime < 1000) {
-            clearInterval(this.intervalId);
-          };
+
+          timIsUp(deltaTime, this.intervalId);
 
           const { days, hours, minutes, seconds } = convertMs(deltaTime);
-          
+
           daysData.textContent = days;
           hoursData.textContent = hours;
           minutesData.textContent = minutes;
           secondsData.textContent = seconds;
         }, 1000);
-      },      
+      },
     };
-
-    btnStart.addEventListener('click', () => {timer.start()})
-    
    
-    // Функція яка додає 0 перед одиничним числом
-    function addLeadingZero(value) {
-      return String(value).padStart(2, '0');
-}
-
-    function convertMs(ms) {
-      // Number of milliseconds per unit of time
-      const second = 1000;
-      const minute = second * 60;
-      const hour = minute * 60;
-      const day = hour * 24;
-
-      const days = addLeadingZero(Math.floor(ms / day));      
-      const hours = addLeadingZero(Math.floor((ms % day) / hour));      
-      const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));      
-      const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
-      return { days, hours, minutes, seconds };
-    }
-      console.dir(selectedDates[0]);
+      console.log(selectedDates[0]);
   },
 };
 
 flatpickr(myInput, options);
 
+// Функція яка додає 0 перед одиничним числом
+    function addLeadingZero(value) {
+      return String(value).padStart(2, '0');
+}
+
+// Конвертує секундиу days, hours, minutes, seconds
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+  return { days, hours, minutes, seconds };
+}
+
+function timIsUp(time, id) {
+  if (time < 1000) {
+    Notify.warning('Time is up', {
+      width: '150px',
+      position: 'center-top',
+    });
+    clearInterval(id);
+  }
+};
+
+function notifyInfo() {
+  Notify.info('Please choose a date in the future', {
+    width: '280px',
+    position: 'center-top',
+  });
+}
