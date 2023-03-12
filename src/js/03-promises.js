2ee19cfa7db178400badee74c6ffa01220e763c5
Sergiy Kushnir <s1983kushnir@gmail.com>
2023-03-12 18:26:33 +0200

    Dun all

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formSub = document.querySelector('.form');
+
+formSub.addEventListener('submit', onSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      }
+      reject({ position, delay });
+    }, delay);
+  });
+};
+
+function onSubmit(e) {
+  e.preventDefault();
+  const form = e.currentTarget;
+  const formData = new FormData(form);
+  const subFormData = {};
+  for (const [key, value] of formData.entries()) {
+    subFormData[key] = Number(value);
+  };
+
+  for (let position = 1; position <= subFormData.amount; position += 1) {
+
+    createPromise(position, subFormData.delay).then(notyFulfild).catch(notyRejact);
+// Оновлюємо subFormData.delay на кожному циклі
+    subFormData.delay = subFormData.delay + subFormData.step;
+  };
+  
+  // form.reset();
+};
+
+function notyRejact({ position, delay }) {
+  Notify.failure(`Rejected promise ${position} in ${delay}ms`);
 }
+
+function notyFulfild({ position, delay }) {
+  Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+}
+
+
+
